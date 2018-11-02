f2583cb96ad9c38b1f9d289cd6f6072aebdd0856
lowlighter <simon.lecoq@live.fr>
2018-11-02 01:35:22 -0400

    Mise à jour du second devoir d'architectures parallèles

--- parallels/2/d2p.c
+++ parallels/2/d2p.c
@@ -3,15 +3,23 @@
   #include <stdlib.h>
   #include <omp.h>
   #include <unistd.h>
+  #include <limits.h>
+
+//Constantes
+  #define INFINITY INT_MAX
 
 //Déclarations
   void parallel_fusion(int* u, int p1, int r1, int p2, int r2, int* t, int p3);
+  void fusion(int* u, int* v, int* t, int p1, int r1, int p2, int r2);
   int n = 0;
+  int limit = 100000;
+  int size = 0;
 
 /** Point d'entrée. */
   int main(int argc, char *argv[]) {
     //Si précisé en argument, on met à jour le nombre de threads voulu
-      if (argc > 1) omp_set_num_threads(atoi(argv[1]));
+      if (argc >= 2) omp_set_num_threads(atoi(argv[1]));
+      if (argc >= 3) limit = atol(argv[2]);
 
     //Lecture du nombre d'entrées
       scanf("%d", &n);
@@ -31,8 +39,14 @@
         #pragma omp single
         { 
           parallel_fusion(w, 0, n, n, 2*n, t, 0); 
-          printf("PARALLEL (%d threads)\r\n", omp_get_num_threads());
-          printf("Temps d'exécution : %lf\r\n", omp_get_wtime() - chrono);
+        }
+        #pragma omp barrier
+        {
+          #pragma omp single
+          {
+            printf("PARALLEL (%d threads)\r\n", omp_get_num_threads());
+            printf("Temps d'exécution : %lf\r\n", omp_get_wtime() - chrono);
+          }
         }
       }
       
@@ -64,19 +78,53 @@
         tmp = p1; p1 = p2; p2 = tmp;
         tmp = r1; r1 = r2; r2 = tmp;
       }
-    
+
     //S'il reste encore des éléments à fusionner, fusion
       if (n1 > 0) {
-        //Calcul des index et affectation de l'élément
-          int q1 = (p1+r1)/2;
-          int q2 = binary_search(w[q1], w, p2, r2);
-          int q3 = p3 + (q1-p1) + (q2-p2);
-          t[q3] = w[q1];
+        //Sequentiel
+          if ((n2 > 0)&&(((r1 - p1) + (r2 - p2)) < limit)) {
+            //Initialisation
+              int* u = (int*) malloc( ((r1 - p1)+1) * sizeof(int));
+              int* v = (int*) malloc( ((r2 - p2)+1) * sizeof(int));
+              int i = 0;
+              for (i = 0 ; i < r1-p1 ; i++) u[i] = w[p1+i];
+              for (i = 0 ; i < r2-p2 ; i++) v[i] = w[p2+i];
+            //Nouvelle tâche
+              #pragma omp task
+                fusion(u, v, t, p1, r1, p2, r2)
+          }
+        //Parallele
+          else {
+            //Calcul des index et affectation de l'élément
+              int q1 = (p1+r1)/2;
+              int q2 = binary_search(w[q1], w, p2, r2);
+              int q3 = p3 + (q1-p1) + (q2-p2);
+              t[q3] = w[q1];
 
-        //Nouvelles tâches
-          #pragma omp task
-            parallel_fusion(w, p1, q1, p2, q2, t, p3);
-          #pragma omp task
-            parallel_fusion(w, q1+1, r1, q2, r2, t, q3+1);
+            //Nouvelles tâches
+              #pragma omp task
+                parallel_fusion(w, p1, q1, p2, q2, t, p3);
+              #pragma omp task
+                parallel_fusion(w, q1+1, r1, q2, r2, t, q3+1);
+          }
+      }
+  }
+
+/** Fonction de fusion où u et v sont fusionnées dans t. */
+  void fusion(int* u, int* v, int* t, int p1, int r1, int p2, int r2) {
+    //Initialisation des itérateurs
+      int i = 0;
+      int j = 0;
+    //Initialisation des valeurs de fin de tableau
+      u[(r1-p1)] = INFINITY;
+      v[(r2-p2)] = INFINITY;
+   
+    //Fusion
+      int k = 0;
+      for (k = p1 ; k < ((r1 - p1)+1) + ((r2 - p2)+1) ; k++) {
+        if (u[i] < v[j]) 
+          t[k] = u[i++];
+        else 
+          t[k] = v[j++];
       }
   }
