47d7305859887bd9f2ecb8b1e0709ebcbe87dbcf
lowlighter <simon.lecoq@live.fr>
2018-11-27 03:26:45 -0500

    Ajout du manifeste

--- run.cmd
+++ run.cmd
@@ -1,5 +1,9 @@
 @echo off
 rmdir build /s /q > nul 2>&1
-mkdir build
-"C:\Program Files\Java\jdk-11.0.1\bin\javac" source/*.java -d build/
-"C:\Program Files\Java\jdk-11.0.1\bin\java" -cp build Engine
+mkdir build > nul 2>&1
+cd source
+"C:\Program Files\Java\jdk-11.0.1\bin\javac" *.java -d ../build
+cd ../build
+"C:\Program Files\Java\jdk-11.0.1\bin\jar" cfm Engine.jar ../source/MANIFEST.mf *.class
+"C:\Program Files\Java\jdk-11.0.1\bin\java" -jar Engine.jar
+cd ..
