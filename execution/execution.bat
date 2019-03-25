f268067d8154efc573bb77c32bc549029d085817
louisquentinjoucla <louis-quentin.joucla@isen.yncrea.fr>
2019-03-25 13:55:29 -0400

    Partie A

--- execution/execution.bat
+++
@@ -1,6 +0,0 @@
-REM L'executable (LaboPSO.exe) doit etre dans le meme repertoire
-
-REM Liste des paramètres
-REM 1)EXECUTABLE 2)TAILLE Essaim 	3)C1 	4)C2 	5)C3 	6)NB_EVAL_MAX
-
-LaboPSO.exe	10			0.1	0.1	0.1	1000
