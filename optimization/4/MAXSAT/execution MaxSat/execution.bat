94655ad7fb71505b6b724ea4ea1f46784c6f70d3
lowlighter <simon.lecoq@live.fr>
2019-04-01 21:25:40 -0400

    Ajout du quatrième devoir d'optimisation

---
+++ optimization/4/MAXSAT/execution MaxSat/execution.bat
@@ -0,0 +1,6 @@
+REM L'executable (LaboPSO_MaxSat.exe) doit etre dans le meme repertoire ainsi que le fichier d'instance
+
+REM Liste des paramètres
+REM 1)EXECUTABLE 2)TAILLE Essaim 	3)C1 	4)C2 	5)C3 	6)NB_EVAL_MAX	7)FICHER
+
+LaboPSO_MaxSat.exe	10		0.1	0.1	0.1	1000		SatTest.txt
